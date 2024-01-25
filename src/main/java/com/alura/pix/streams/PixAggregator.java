a540af4cddf89672fd18baee77210ebf66b472ef
CarlosBotelho42 <carlospiece55@gmail.com>
2024-01-24 23:54:59 -0300

    feat: trabalhando com o kafka aggregator para pegar os pix de um id e ir emcrementando a cada transacao

--- src/main/java/com/alura/pix/streams/PixAggregator.java
+++ src/main/java/com/alura/pix/streams/PixAggregator.java
@@ -5,10 +5,7 @@ import com.alura.pix.dto.PixDTO;
 import com.alura.pix.serdes.PixSerdes;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Printed;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,13 +15,18 @@ public class PixAggregator {
     @Autowired
     public void aggregator(StreamsBuilder streamsBuilder){
 
-        KStream<String, PixDTO> messageStrem = streamsBuilder
+        KTable<String, Double> messageStrem = streamsBuilder
                 .stream("pix-topic", Consumed.with(Serdes.String(), PixSerdes.serdes()))
                 .peek((key, value) -> System.out.printf("Pix recebido: " + value.getChaveOrigem()))
-                .filter((key, value) -> value.getValor() > 1000);
+                .groupBy((key, value) -> value.getChaveOrigem())
+                .aggregate(
+                        () -> 0.0,
+                        (key, value, aggregate) -> (aggregate + value.getValor()),
+                        Materialized.with(Serdes.String(), Serdes.Double())
+                );
 
-        messageStrem.print(Printed.toSysOut());
-        messageStrem.to("pix-topic-verifica-fraude", Produced.with(Serdes.String(), PixSerdes.serdes()));
+        messageStrem.toStream().print(Printed.toSysOut());
+        messageStrem.toStream().to("pix-topic-agregacao", Produced.with(Serdes.String(), Serdes.Double()));
 
     }
 
